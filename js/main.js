c6d1bc0e4e00e1298fd916bdd5045250a795a089
Ilya Shishkin <mostbook@mail.ru>
2022-10-12 10:59:39 +0700

    Moved news mock data to a separate file. Moved all initialization logic to the start() function.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,15 @@
 'use strict';
 
 import { NewsElem } from './view.js';
+import { news } from './mock-news.js';
 
-const news = [
-  {
-    title: 'Новость 1',
-    preview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi deserunt dicta dignissimos, ipsam iste minima molestiae quod sequi voluptatum?',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet asperiores aut nihil! Corporis debitis labore fugiat id, eligendi ratione veritatis!\nLorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic, ipsa, ullam, cupiditate eveniet at voluptate corrupti commodi nobis ratione voluptatem! Vel animi totam cupiditate doloribus ad ab exercitationem officia eveniet impedit? Deleniti quasi nisi consectetur perspiciatis quibusdam nostrum, enim perferendis nam, magni molestias recusandae id libero vitae, repudiandae praesentium.',
-  },
-  {
-    title: 'Новость 2',
-    preview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi deserunt dicta dignissimos, ipsam iste minima molestiae quod sequi voluptatum?',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet asperiores aut nihil! Corporis debitis labore fugiat id, eligendi ratione veritatis!\nLorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic, ipsa, ullam, cupiditate eveniet at voluptate corrupti commodi nobis ratione voluptatem! Vel animi totam cupiditate doloribus ad ab exercitationem officia eveniet impedit? Deleniti quasi nisi consectetur perspiciatis quibusdam nostrum, enim perferendis nam, magni molestias recusandae id libero vitae, repudiandae praesentium.',
-  },
-  {
-    title: 'Новость 3',
-    preview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi deserunt dicta dignissimos, ipsam iste minima molestiae quod sequi voluptatum?',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet asperiores aut nihil! Corporis debitis labore fugiat id, eligendi ratione veritatis!\nLorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic, ipsa, ullam, cupiditate eveniet at voluptate corrupti commodi nobis ratione voluptatem! Vel animi totam cupiditate doloribus ad ab exercitationem officia eveniet impedit? Deleniti quasi nisi consectetur perspiciatis quibusdam nostrum, enim perferendis nam, magni molestias recusandae id libero vitae, repudiandae praesentium.',
-  },
-];
+const start = () => {
+  const root = document.querySelector('#root');
 
-const start = ( rootElem ) => {
   news.forEach(item => {
     const newNews = new NewsElem(item);
-    rootElem.append(newNews);
+    root.append(newNews);
   });
 };
 
-const root = document.querySelector('#root');
-start(root);
+start();
---
+++ js/mock-news.js
@@ -0,0 +1,17 @@
+export const news = [
+  {
+    title: 'Новость 1',
+    preview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi deserunt dicta dignissimos, ipsam iste minima molestiae quod sequi voluptatum?',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet asperiores aut nihil! Corporis debitis labore fugiat id, eligendi ratione veritatis!\nLorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic, ipsa, ullam, cupiditate eveniet at voluptate corrupti commodi nobis ratione voluptatem! Vel animi totam cupiditate doloribus ad ab exercitationem officia eveniet impedit? Deleniti quasi nisi consectetur perspiciatis quibusdam nostrum, enim perferendis nam, magni molestias recusandae id libero vitae, repudiandae praesentium.',
+  },
+  {
+    title: 'Новость 2',
+    preview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi deserunt dicta dignissimos, ipsam iste minima molestiae quod sequi voluptatum?',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet asperiores aut nihil! Corporis debitis labore fugiat id, eligendi ratione veritatis!\nLorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic, ipsa, ullam, cupiditate eveniet at voluptate corrupti commodi nobis ratione voluptatem! Vel animi totam cupiditate doloribus ad ab exercitationem officia eveniet impedit? Deleniti quasi nisi consectetur perspiciatis quibusdam nostrum, enim perferendis nam, magni molestias recusandae id libero vitae, repudiandae praesentium.',
+  },
+  {
+    title: 'Новость 3',
+    preview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi deserunt dicta dignissimos, ipsam iste minima molestiae quod sequi voluptatum?',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet asperiores aut nihil! Corporis debitis labore fugiat id, eligendi ratione veritatis!\nLorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic, ipsa, ullam, cupiditate eveniet at voluptate corrupti commodi nobis ratione voluptatem! Vel animi totam cupiditate doloribus ad ab exercitationem officia eveniet impedit? Deleniti quasi nisi consectetur perspiciatis quibusdam nostrum, enim perferendis nam, magni molestias recusandae id libero vitae, repudiandae praesentium.',
+  },
+];
